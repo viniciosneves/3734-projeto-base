307a1ece073efc4581b43e0566ed853c6fc88b6f
Vinicios Neves <vinicios_neves@hotmail.com>
2024-02-19 15:25:17 +0000

    aula 3: desafio resolvido

--- src/components/CommentList/index.jsx
+++ src/components/CommentList/index.jsx
@@ -1,12 +1,15 @@
 import { Comment } from "../Comment"
+import { Replies } from "../Replies"
+import styles from './commentlist.module.css'
 
 export const CommentList = ({ comments }) => {
     return (
-        <section>
+        <section className={styles.comments}>
             <h2>Comentários</h2>
             <ul>
                 {comments.map(comment => <li key={comment.id}>
                     <Comment comment={comment} />
+                    <Replies />
                 </li>)}
             </ul>
         </section>
---
+++ src/components/Replies/index.jsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useState } from 'react'
+import styles from './replies.module.css'
+
+export const Replies = ({ comment }) => {
+
+    const [showReplies, setShowReplies] = useState(false)
+
+    return (<div className={styles.container}>
+
+        <div className={styles.replies}>
+            <button className={styles.btn} onClick={() => setShowReplies(!showReplies)}>
+                {showReplies ? 'Ocultar' : 'Ver'} respostas
+            </button>
+        </div>
+    </div>)
+}
